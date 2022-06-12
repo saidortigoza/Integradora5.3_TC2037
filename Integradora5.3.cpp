fb95acda8f1e4282b08b60d344c39bb2b153c0bc
Miguel Tang <A01610836@itesm.mx>
2022-06-11 20:23:53 -0500

    add reading input files for sequential

--- Integradora5.3.cpp
+++ Integradora5.3.cpp
@@ -18,8 +18,17 @@ const int threads = 8;
 using namespace std;
 
 // Resaltador Secuencial
-void resaltadorSeq(string a, int size) {
-
+void resaltadorSeq(string *rutas, int size) {
+	for (int i = 0; i < size; i++) {
+		string line;
+		ifstream inputFile(rutas[i]);
+		if (inputFile.is_open()) {
+			while (getline(inputFile, line)) {
+				cout << line << endl;
+				//Manipulacion del archivo
+			}
+		}
+	}
 }
 
 typedef struct {
@@ -27,7 +36,7 @@ typedef struct {
 	string* arr;
 } Block;
 
-//Resaltador Secuencial
+//Resaltador Concurrente
 void* resaltadorConc(void* args) {
 	Block *block;
 	block = (Block*)args;
@@ -36,40 +45,45 @@ void* resaltadorConc(void* args) {
 		ifstream inputFile(block->arr[i]);
 		if (inputFile.is_open()) {
 			while (getline(inputFile, line)) {
-				//cout << line << endl;
+				cout << line << endl;
 				//Manipulacion del archivo
 			}
 		}
 		inputFile.close();
-		
-		//cout << block->arr[i] << endl;
 	}
 }
 
 int main(int argc, char* argv[]) {
 	pthread_t th[threads];
-	double ms;
+	double msSeq, msConc;
 	Block blocks[threads];
 	int block_size, i, j;
-	string *fileRoutes;
+	string *rutasArchivos;
 
 	const int size_Arr = argc;
 
-	fileRoutes = new string[size_Arr];
+	rutasArchivos = new string[size_Arr];
 
 	for (i = 0; i < argc; i++) {
-		fileRoutes[i] = argv[i];
+		rutasArchivos[i] = argv[i];
 	}
+	//Manera secuencial
 
+	msSeq = 0;
+	start_timer();
+	resaltadorSeq(rutasArchivos, size_Arr);
+	msSeq = stop_timer();
+
+	//Manera Concurrente
 	block_size = size_Arr / threads;
 
 	for (i = 0; i < threads; i++) {
-		blocks[i].arr = fileRoutes;
+		blocks[i].arr = rutasArchivos;
 		blocks[i].start = i * block_size;
 		blocks[i].end = i != (threads - 1) ? (i + 1) * block_size : size_Arr;
 	}
 
-	ms = 0;
+	msConc = 0;
 	for (j = 0; j < N; j++) {
 		start_timer();
 
@@ -84,11 +98,13 @@ int main(int argc, char* argv[]) {
 				perror("Fallo al unir hilo");
 			}
 		}
-		ms += stop_timer();
+		msConc += stop_timer();
 	}
-	
-	cout << "------------MultiThread--------------\n";
-	cout << "avg time = " << setprecision(5) << (ms / N) << "\n";
+
+	cout << "------------Secuencial--------------\n";
+	cout << "Tiempo = " << setprecision(5) << (msSeq / N) << "\n";
+	cout << "------------Multihilo--------------\n";
+	cout << "Tiempo Promedio = " << setprecision(5) << (msConc / N) << "\n";
 
 
 	return 0;
